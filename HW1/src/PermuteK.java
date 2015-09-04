d7d743614f0ce747d913330167a8948861f1aa9f
Chinthiti Wisetsombat <chinno_ne@hotmail.com>
2015-09-04 16:47:07 +0700

    fix permuteK cutstring stackoverflow

--- HW1/src/PermuteK.java
+++ HW1/src/PermuteK.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class PermuteK {
@@ -10,19 +11,18 @@ public class PermuteK {
 		strings.add(string);
 		
 		strings = permuteK(strings, length, 0);
-		cutString(strings, length, 0);
+		cutString(strings, length);
 		
 		return strings;
 		
 	}
 	
-	public static void cutString(ArrayList<String> strings, int length, int index){
-		if(index == strings.size()) return;
-		String string = strings.get(index);
-		string = string.substring(string.length()-length);
-		strings.set(index, string);
-		cutString(strings, length, index+1);
-		
+	public static void cutString(ArrayList<String> strings, int length){
+		for(int i = 0; i < strings.size(); i++){
+			String temp = strings.get(i);
+			temp = temp.substring(temp.length()-length);
+			strings.set(i, temp);
+		}
 	}
 	
 	public static ArrayList<String> permuteK(ArrayList<String> strings, int length, int level){
@@ -69,12 +69,17 @@ public class PermuteK {
 	
 	
 	public static void main(String[]args){
-		ArrayList<String> s = permuteK("abcdef", 3);
-		s.sort(String.CASE_INSENSITIVE_ORDER);
-		for(String a : s){
-			System.out.println(a);
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("String input : ");
+		String input = scanner.nextLine();
+		System.out.print("How many character do you want? : ");
+		int num = Integer.parseInt(scanner.nextLine());
+		ArrayList<String> permuted = permuteK(input, num);
+		permuted.sort(String.CASE_INSENSITIVE_ORDER);
+		for(String s : permuted){
+			System.out.println(s);
 		}
-		System.out.println(s.size());
+		System.out.println("Number of results : " + permuted.size());
 		
 	}
 }
